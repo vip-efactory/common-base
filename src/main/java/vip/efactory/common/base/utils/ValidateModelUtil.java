eb7a34c756a6a9c73764f9b897fbda9c2f4ff5f0
dbdu <1797890817@qq.com>
2020-08-20 21:50:48 +0800

    优化属性校验国际化，支持使用Hibernate validator默认的资源文件

--- src/main/java/vip/efactory/common/base/utils/ValidateModelUtil.java
+++ src/main/java/vip/efactory/common/base/utils/ValidateModelUtil.java
@@ -3,11 +3,11 @@ package vip.efactory.common.base.utils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.ValidationUtils;
 import vip.efactory.common.i18n.service.ILocaleMsgSourceService;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
+import javax.validation.*;
 import java.util.*;
 
 /**
@@ -40,19 +40,29 @@ public class ValidateModelUtil {
 
         while (iter.hasNext()) {
             ConstraintViolation<Object> currentObj = iter.next();   //再执行一次就会跳到下一个对象
-            String message = currentObj.getMessage();      //校验要显示的错误信息
-            String property = currentObj.getPropertyPath().toString();    //错误信息对应的字段名
+            // 判断是否有用户自定义的message值,如果有就进行处理，如果没有就进行获取默认的国际化对应的key然后手动处理以期达到跟随国际化区域变化的效果
+            // 得到检查注解里的参数
+            Map<String, Object> params = currentObj.getConstraintDescriptor().getAttributes();
+            // 校验注解的message属性是否y
+            String messagePropertyValue = (String) params.get("message");
+            // 注意:currentObj.getMessage()得到的信息是message属性用户定义的值或者是已经默认的国际化的值，不是我们想要跟随国际化环境变化的值。
+            String message = messagePropertyValue;
+            // 错误信息对应的字段名
+            String property = currentObj.getPropertyPath().toString();
 
             // log.info("property：" + property + ",check failed:" + message);
 
-            //处理属性切分,例如：message="AAA{student.age}BBB {property.not.allow.negative}CCCC"
-            if (message.contains("{") && message.contains("}")) {  //说明包含占位符{}
-                String[] rawKeys = message.split("}");//得到：AAA{student.age、BBB {property.not.allow.negative、CCCC
+            // 处理属性切分,例如：message="AAA{student.age}BBB {property.not.allow.negative}CCCC"
+            // 说明包含占位符{}
+            if (message.contains("{") && message.contains("}")) {
+                //得到：AAA{student.age、BBB {property.not.allow.negative、CCCC
+                String[] rawKeys = message.split("}");
                 // 得到所有的key
                 List<String> keys = new ArrayList<>();
                 for (String rawKey : rawKeys) {
                     if (rawKey.contains("{")) {
-                        String key = rawKey.substring(rawKey.lastIndexOf("{") + 1); //获取key,例如：student.age
+                        //获取key,例如：student.age
+                        String key = rawKey.substring(rawKey.lastIndexOf("{") + 1);
                         keys.add(key);
                     }
                 }
@@ -68,8 +78,7 @@ public class ValidateModelUtil {
 
                 // 替换message 里国际化信息里的占位符,使用注解自带的属性值占位，例如：{min}-{max}
                 if (message.contains("{") && message.contains("}")) {
-                    // 得到检查注解里的参数，以便再次替换模板里的占位符
-                    Map<String, Object> params = currentObj.getConstraintDescriptor().getAttributes();
+                    // 再次替换模板里的占位符
                     for (Map.Entry param : params.entrySet()) {
                         message = message.replace("{" + param.getKey() + "}", param.getValue().toString());
                     }
