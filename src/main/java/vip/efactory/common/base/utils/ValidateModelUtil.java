2922a63e41bc065ced5225837b4734f9719bf8a7
dbdu <1797890817@qq.com>
2020-08-21 12:39:58 +0800

    优化属性校验国际化:实现如果校验注解没有设定自定义的message属性值,默认的校验消息也自动跟国际化变化而变化，从而简化校验注解的使用! 依赖:<hibernate.validator.version>6.1.5.Final</hibernate.validator.version>

--- src/main/java/vip/efactory/common/base/utils/ValidateModelUtil.java
+++ src/main/java/vip/efactory/common/base/utils/ValidateModelUtil.java
@@ -1,10 +1,13 @@
 package vip.efactory.common.base.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
+import org.hibernate.validator.resourceloading.PlatformResourceBundleLocator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-import org.springframework.validation.ValidationUtils;
+import vip.efactory.common.base.valid.LocalizedMessageInterpolator;
 import vip.efactory.common.i18n.service.ILocaleMsgSourceService;
 
 import javax.validation.*;
@@ -20,33 +23,50 @@ import java.util.*;
 public class ValidateModelUtil {
     private static ILocaleMsgSourceService localeMessageSourceService;
 
-    //通过下面的方法为静态成员赋值!!!
+    /**
+     * 通过下面的方法为静态成员赋值!!!
+     *
+     * @param localeMessageSourceService 国际化消息服务
+     */
     @Autowired
     public void setLocaleMessageSourceService(ILocaleMsgSourceService localeMessageSourceService) {
         ValidateModelUtil.localeMessageSourceService = localeMessageSourceService;
     }
 
 
-    //验证某一个对象,可以指定激活哪个校验组，例如Update.class
-    public static Map<String, String> validateModel(Object obj,Class<?>... groups) {
+    /**
+     * 验证某一个对象,可以指定激活哪个校验组，例如Update.class
+     *
+     * @param obj    被验证的对象
+     * @param groups 使用的验证组
+     * @return 返回的错误信息
+     */
+    public static Map<String, String> validateModel(Object obj, Class<?>... groups) {
 
         //用于存储验证后的错误信息
         Map<String, String> errors = new TreeMap<>();
-        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+        // Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        // 这种方式如果校验注解，用户没有指定自定义的信息，框架就会取JVM默认的国际化区域参数值，不是我们想要的那种跟随Header头变化的国际化区域方式
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        // 这里使用我们自己定义的LocalizedMessageInterpolator消息模板解释器，从而达到获取的校验属性跟随国际化区域变化而变化的目的
+        // 此处参考:https://developer.ibm.com/zh/technologies/java/articles/j-cn-hibernate-validator/
+        MessageInterpolator interpolator = new LocalizedMessageInterpolator(new ResourceBundleMessageInterpolator(new PlatformResourceBundleLocator("ValidationMessages")), LocaleContextHolder.getLocale());
+        Validator validator = factory.usingContext().messageInterpolator(interpolator).getValidator();
 
         //验证某个对象,，其实也可以只验证其中的某一个属性的
         Set<ConstraintViolation<Object>> constraintViolations = validator.validate(obj, groups);
         Iterator<ConstraintViolation<Object>> iter = constraintViolations.iterator();
 
         while (iter.hasNext()) {
-            ConstraintViolation<Object> currentObj = iter.next();   //再执行一次就会跳到下一个对象
+            // 再执行一次就会跳到下一个对象
+            ConstraintViolation<Object> currentObj = iter.next();
             // 判断是否有用户自定义的message值,如果有就进行处理，如果没有就进行获取默认的国际化对应的key然后手动处理以期达到跟随国际化区域变化的效果
             // 得到检查注解里的参数
             Map<String, Object> params = currentObj.getConstraintDescriptor().getAttributes();
-            // 校验注解的message属性是否y
-            String messagePropertyValue = (String) params.get("message");
-            // 注意:currentObj.getMessage()得到的信息是message属性用户定义的值或者是已经默认的国际化的值，不是我们想要跟随国际化环境变化的值。
-            String message = messagePropertyValue;
+            // 注意:currentObj.getMessage()得到的信息是message属性用户定义的值或者是已经默认的国际化的值。
+            String message = currentObj.getMessage();
             // 错误信息对应的字段名
             String property = currentObj.getPropertyPath().toString();
 
@@ -68,7 +88,7 @@ public class ValidateModelUtil {
                 }
                 // 替换message里的占位符
                 for (String key : keys) {
-                    String value = localeMessageSourceService.getMessage(key);
+                    String value = localeMessageSourceService.getMessage(key, LocaleContextHolder.getLocale());
                     if (StringUtils.isEmpty(value) || value.equals(key)) {
                         value = key;
                         log.warn("missing key [{}]", key);
